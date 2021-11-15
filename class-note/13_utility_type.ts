633e92ce38d1032a34f88b4b450f4f6c75733d23
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-11-15 15:26:38 +0900

    Update implement Utillity

--- class-note/13_utility_type.ts
+++ class-note/13_utility_type.ts
@@ -35,5 +35,41 @@ function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price
 type UpdateProduct  = Partial<Product>
 // 3. 특정 상품 정보를 업데이트(갱신)하는 함수
 function updatePrudictItem(productItem : Partial<Product>) {
-    
+
+}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+    username : string;
+    email : string;
+    profilePhotoUrl : string;
+}
+
+// interface UserProfileUpdate {
+//     username? : string;
+//     email? : string;
+//     profilePhotoUrl? : string;
+// }
+
+// #1
+// type UserProfileUpdate = {
+//     username?: UserProfile['username']; 
+//     email?:  UserProfile['email'];
+//     profilePhotoUrl?:  UserProfile['profilePhotoUrl'];
+// }
+
+// #2 맵드 타입(Mapped Type)
+// type UserProfileUpdate = {
+//     [p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p]
+// }
+// type UserPrifileKeys = keyof UserProfile
+
+// #3
+type UserProfileUpdate = {
+    [p in keyof UserProfile]?: UserProfile[p]
+}
+
+// #4
+type Subset<T> = {
+    [p in keyof T]?: T[p]
 }
