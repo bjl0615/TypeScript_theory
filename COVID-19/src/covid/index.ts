48e260188b02415f80a806c9eb71d0eef2416489
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-11-21 00:27:33 +0900

    Add API function Type

---
+++ COVID-19/src/covid/index.ts
@@ -0,0 +1,45 @@
+interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: '9a4dce17-405a-4efb-8f00-7a58e005783b';
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface Global {
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+export interface CovideSummaryResonse {
+  Countries: Country[];
+  Date: string;
+  Global: Global;
+  Message: string;
+}
+
+interface CountrySummaryInfo {
+  Cases: number;
+  City: string;
+  CityCode: string;
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  Lat: string;
+  Lon: string;
+  Province: string;
+  Status: string;
+}
+
+export type CountrySummaryResponse = CountrySummaryInfo[];
